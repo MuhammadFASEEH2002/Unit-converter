658227948f0fc647c563ba7957b709e40d931d5f
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2023-05-01 15:56:04 +0500

    1/5/2023 (Added Responsiveness)

--- script.js
+++ script.js
@@ -28,20 +28,43 @@ var number=document.getElementById('input').value;
     var kelvin2 = document.getElementById("Kelvin1");
     var celsius2 = document.getElementById("Celsius1");
     var fahrenheit2 = document.getElementById("Fahrenheit1");
-    var number=document.getElementById('input').value;
-    if (kelvin1.checked && kelvin2.checked){
-      document.getElementById('answer').innerText=`Ans is ${number}`;
+    var num=document.getElementById('input').value;
+    var number=parseInt(num);
+    if (kelvin1.checked && kelvin2.checked){  //converting Kelvin into Kelvin
+      document.getElementById('answer').innerText=`${number} K`;
      } 
-     else if(kelvin1.checked && celsius2.checked) {
-        result=number-273;
-        document.getElementById('answer').innerText=`Ans is ${result} C`;
+     else if(kelvin1.checked && celsius2.checked) {    //converting Kelvin into Celsius
+        result=(number - 273.15);
+        document.getElementById('answer').innerText=`${result} C`;
      } 
-     else if(kelvin1.checked && fahrenheit2.checked) {
-       var result=(((number-273.15)*(9/5))+32);
-       result.toFixed(2);
-       document.getElementById('answer').innerText=`Ans is ${result} C`;
+     else if(kelvin1.checked && fahrenheit2.checked) {      //converting Kelvin into Fahrenheit
+       result=(((number-273.15)*(9/5))+32);
+       document.getElementById('answer').innerText=`${result} F`;
      }
+     else if(celsius1.checked && kelvin2.checked){   //converting Celsius into Kelvin
+      var kelvin_unit=273.15;
+      result= number + kelvin_unit;
+      document.getElementById('answer').innerText=`${result} K`;
+     }
+     else if(celsius1.checked && celsius2.checked) {     //converting Celsius into Celsius
+      document.getElementById('answer').innerText=`${number} C`;
+     } 
+     else if(celsius1.checked && fahrenheit2.checked) {   //converting Celsius into Fahrenheit
+       result=((number*(9/5))+32);
+      document.getElementById('answer').innerText=`${result} F`;
+    }
+     else if(fahrenheit1.checked && kelvin2.checked){   //converting Fahrneheit into Kelvin
+      result=(((number - 32) *(5/9)) + 273.15);
+      document.getElementById('answer').innerText=`${result} K`;
+     }
+     else if(fahrenheit1.checked && celsius2.checked) {    //converting Fahrneheit into Celsius
+      result=((number - 32) * (5/9));
+      document.getElementById('answer').innerText=`${result} C`;
+     } 
+     else if(fahrenheit1.checked && fahrenheit2.checked) {   //converting Fahrneheit into Fahrenheit
+      document.getElementById('answer').innerText=`${number} F`;
+   }
   }
+  //calling the function on button click
   convertButton=document.getElementById('convert_button');
-  
   convertButton.addEventListener('click', checkForInputUnit);
